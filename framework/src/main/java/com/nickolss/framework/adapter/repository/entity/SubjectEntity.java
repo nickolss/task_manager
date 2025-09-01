b3059e24f799c5fff5a06fa5fef97232a09f70d1
nickolss <nickolasmaiaraujo@gmail.com>
2025-09-01 13:54:24 -0300

    feat: added simple database connection

---
+++ framework/src/main/java/com/nickolss/framework/adapter/repository/entity/SubjectEntity.java
@@ -0,0 +1,37 @@
+package com.nickolss.framework.adapter.repository.entity;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+public class SubjectEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ framework/src/main/java/com/nickolss/framework/adapter/repository/entity/TaskEntity.java
@@ -0,0 +1,84 @@
+package com.nickolss.framework.adapter.repository.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+public class TaskEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Date startDate;
+
+    @Column
+    private Date endDate;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private Date createAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
