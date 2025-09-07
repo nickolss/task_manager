a4406dd91128d17925666436e6a010607145acd8
nickolss <nickolasmaiaraujo@gmail.com>
2025-09-07 11:06:36 -0300

    feat: added docker in project

--- backend/framework/src/main/java/com/nickolss/framework/useCase/AuthService.java
+++ backend/framework/src/main/java/com/nickolss/framework/useCase/AuthService.java
@@ -1,7 +1,9 @@
 package com.nickolss.framework.useCase;
 
+import org.springframework.stereotype.Service;
 import service.AuthInterface;
 
+@Service
 public class AuthService implements AuthInterface {
     @Override
     public void register(String name, String email, String password) {
