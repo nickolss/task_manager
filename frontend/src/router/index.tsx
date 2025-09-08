b44abab64a9aff01276cf4714ada570161266ffd
nickolss <nickolasmaiaraujo@gmail.com>
2025-09-08 16:03:18 -0300

    feat: added simple dashboard page

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LoginPage from '@/pages/LoginPage';
 import RegisterPage from '@/pages/RegisterPage';
+import DashboardPage from '@/pages/DashboardPage';
 
 export const router = createBrowserRouter([
   {
@@ -11,4 +12,8 @@ export const router = createBrowserRouter([
     path: '/register',
     element: <RegisterPage />,
   },
+  {
+    path: "/",
+    element: <DashboardPage />,
+  }
 ]);
